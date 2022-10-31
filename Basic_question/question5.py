5505181971909c2b75b8a0d3027baea59b112201
Aniket-Dilip-Panse <aniketpanse12@gmail.com>
2022-11-01 00:14:56 +0530

    printing firstname and lastname in reverse order with space

---
+++ Basic_question/question5.py
@@ -0,0 +1,4 @@
+#  Write a Python program which accepts the user's first and last name and print them in reverse order with a space between them.
+first_name = input('enter your first name ')
+last_name = input('enter your last name ')
+print(f"namaskar {last_name} {first_name}")
