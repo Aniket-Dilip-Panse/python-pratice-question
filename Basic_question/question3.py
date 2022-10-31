8a6c37e74b2a6e72cba75fe7f0987b24b7dbcbb2
Aniket-Dilip-Panse <aniketpanse12@gmail.com>
2022-11-01 00:00:21 +0530

    printing area of circle

---
+++ Basic_question/question3.py
@@ -0,0 +1,5 @@
+#  Write a Python program which accepts the radius of a circle from the user and compute the area.
+def Circle_area():
+    radius = input('enter the radius of circle ')
+    area = 3.142 * radius * radius
+    print(area)
