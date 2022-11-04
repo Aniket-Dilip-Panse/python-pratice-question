d6cf8ec546f65da5f28a342c666498f3cb7ae4a9
Aniket-Dilip-Panse <aniketpanse12@gmail.com>
2022-11-04 09:44:17 +0530

    test a number between 100 of 1000 or 2000

--- Basic_question/question12.py
+++ Basic_question/question12.py
@@ -1,8 +1,8 @@
 # Write a Python program to print the calendar of a given month and year.
 import calendar
-# year = int(input('Enter the year for which you want the calender '))
-# month = int(input('Enter the month for which you want the calender '))
-# print(calendar.month(year,month))
+year = int(input('Enter the year for which you want the calender '))
+month = int(input('Enter the month for which you want the calender '))
+print(calendar.month(year,month))
 
 # to print whole calender of any year use below code calender.calender(year)
 print(calendar.calendar(2001))
