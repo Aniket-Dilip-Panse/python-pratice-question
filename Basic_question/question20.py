9c09ca54db8a6b87697b099db85c694b97191fb7
Aniket-Dilip-Panse <aniketpanse12@gmail.com>
2022-11-04 10:37:11 +0530

    even or odd

---
+++ Basic_question/question20.py
@@ -0,0 +1,6 @@
+# Write a Python program to find whether a given number (accept from the user) is even or odd, print out an appropriate message to the user.
+input_number = int(input('enter any number '))
+if input_number % 2 == 0:
+    print('the inputed number is even ')
+else:
+    print('The inputed number is odd ')
