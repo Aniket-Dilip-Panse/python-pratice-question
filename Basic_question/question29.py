68bacd0affc02fe6f9e1ec58da845831aaf826c8
Aniket-Dilip-Panse <aniketpanse12@gmail.com>
2022-11-06 23:47:36 +0530

    triangle area

---
+++ Basic_question/question29.py
@@ -0,0 +1,4 @@
+# Write a Python program that will accept the base and height of a triangle and compute the area
+def triangle_area(base,height):
+    return 0.5 * base * height
+print(triangle_area(1,3))
